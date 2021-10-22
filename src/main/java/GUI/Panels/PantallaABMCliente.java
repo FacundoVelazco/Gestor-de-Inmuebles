3446d5ed87dffd98f3cefca51de0a520704e6277
brunoagretti <brunoagretti@hotmail.com>
2021-10-22 10:47:58 -0300

    Mejorada la busqueda y añadido listeners a la tabla

--- src/main/java/GUI/Panels/PantallaABMCliente.java
+++ src/main/java/GUI/Panels/PantallaABMCliente.java
@@ -4,27 +4,30 @@ import GUI.GestorGUI;
 import GUI.Pantalla;
 
 import javax.swing.*;
+import javax.swing.event.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class PantallaABMCliente {
 
     private JPanel panelPrincipal;
     private JLabel titulo;
     private JTable tablaClientes;
+    private TableRowSorter sorter;
     private JButton botonEliminar;
     private JButton botonModificar;
     private JButton botonNuevoCliente;
     private JScrollPane scrollPane;
     private JPanel panelBusqueda;
-    private JTextField textFieldNombre;
-    private JLabel textoUsuario;
-    private JLabel labelNombreApellido;
-    private JTextField textFieldNombreApellido;
-    private JButton buttonBuscar;
+    private JTextField textFieldBuscar;
+    private JLabel textoBuscar;
     private JPanel panelTitulo;
     private JPanel panelControles;
+    private JButton buttonLimpiar;
 
     private DataModel dataModel = new DataModel(DATA, COLUMNS);
     private static final String[] COLUMNS = {"Nombre", "Apellido","Nombre de usuario"};
@@ -52,6 +55,8 @@ public class PantallaABMCliente {
         tablaClientes.getTableHeader().setReorderingAllowed(false);
         tablaClientes.getTableHeader().setResizingAllowed(false);
         tablaClientes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        sorter = new TableRowSorter<>(dataModel);
+        tablaClientes.setRowSorter(sorter);
 
         botonNuevoCliente.addActionListener(new ActionListener() {
             @Override
@@ -59,11 +64,60 @@ public class PantallaABMCliente {
                 GestorGUI.push(Pantalla.CREAR_CLIENTE);
             }
         });
+        textFieldBuscar.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                search(textFieldBuscar.getText());
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                search(textFieldBuscar.getText());
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                search(textFieldBuscar.getText());
+            }
+
+            public void search(String str) {
+                tablaClientes.clearSelection();
+                botonesActivados(false);
+
+                if (str.length() == 0) {
+                    sorter.setRowFilter(null);
+                } else {
+                    sorter.setRowFilter(RowFilter.regexFilter("(?i)" + str));
+                }
+            }
+        });
+        buttonLimpiar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textFieldBuscar.setText("");
+            }
+        });
+        tablaClientes.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (tablaClientes.getSelectedRow()>-1 && e.getValueIsAdjusting()){
+                    System.out.println("Cliente seleccionado: " +
+                            dataModel.getValueAt(tablaClientes.convertRowIndexToModel(tablaClientes.getSelectedRow()),0));
+                    botonesActivados(true);
+                }
+            }
+        });
+
+    }
+
+    /** Activa o desactiva los botones de Eliminar y Modificar */
+    private void botonesActivados(Boolean b){
+        botonEliminar.setEnabled(b);
+        botonModificar.setEnabled(b);
     }
 
     public JPanel getPanelPrincipal() {
         return panelPrincipal;
     }
-
 
 }
