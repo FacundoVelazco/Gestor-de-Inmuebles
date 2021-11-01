9dde1fae6dd30da2b8aa8826771d065374f850e1
AgusIGarcia <garagus99@gmail.com>
2021-10-31 22:48:02 -0300

    Agregamos algunos botones en la interfaz y planteamos listeners, falta gestionar la transicion de paneles

---
+++ src/main/java/GUI/Panels/AMInmueblePanels/PanelUbicacion.java
@@ -0,0 +1,12 @@
+package GUI.Panels.AMInmueblePanels;
+
+import javax.swing.*;
+
+public class PanelUbicacion {
+    private JPanel panelUbicacion;
+    private JLabel tituloLabel;
+
+    public JPanel getPanelUbicacion() {
+        return panelUbicacion;
+    }
+}
--- src/main/java/GUI/Panels/PantallaAMInmueble.java
+++ src/main/java/GUI/Panels/PantallaAMInmueble.java
@@ -1,13 +1,41 @@
 package GUI.Panels;
 
+import GUI.Panels.AMInmueblePanels.PanelUbicacion;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PantallaAMInmueble {
     private JPanel panelPrincipal;
     private JPanel panelTitulos;
-    private JLabel titulo;
+    private JLabel tituloLabel;
+    private JPanel panelRotativo;
+    private JPanel panelNoModificables;
+    private JLabel codigoLabel;
+    private JLabel estadoLabel;
+    private JLabel fechaDeCargaLabel;
+    private JPanel panelBotonesInferiores;
+    private JButton botonCancelar;
+    private JButton botonAnterior;
+    private JButton botonSiguiente;
 
     public JPanel getPanelPrincipal() {
         return panelPrincipal;
+    }
+
+
+    public PantallaAMInmueble() {
+
+        panelRotativo = new PanelUbicacion().getPanelUbicacion();
+
+        botonSiguiente.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panelRotativo = new PanelUbicacion().getPanelUbicacion();
+               // panelRotativo.set
+                System.out.println("Hice algo xd");
+            }
+        });
     }
 }
